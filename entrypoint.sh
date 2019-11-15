db93eb4ed642878b0e15452505ddab67325989f4
Martin Simon <me@martinsimon.me>
2019-11-15 17:20:52 +0100

    v1.2.0 - catch empty ENV vars, fix curl exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,39 @@
 set -e
 
 METADATA=$(cat metadata.json)
-RELEASE_NAME=$(echo "${METADATA}" | jq -r .name)
-RELEASE_VERSION=$(echo "${METADATA}" | jq -r .version)
-NAME="pkg/${RELEASE_NAME}-${RELEASE_VERSION}"
+RELEASE_NAME=$(echo "$METADATA" | jq -r .name)
+RELEASE_VERSION=$(echo "$METADATA" | jq -r .version)
+NAME="pkg/$RELEASE_NAME-$RELEASE_VERSION"
+
+if [ -z "$FORGE_API_KEY" ]; then
+    echo "FORGE_API_KEY environment variable is not set, exiting..."
+    exit 1
+fi
+
+if [ -z "$REPOSITORY_URL" ]; then
+    echo "REPOSITORY_URL environment variable is not set, using Puppet Forge."
+    REPOSITORY_URL='https://forgeapi.puppet.com/v3/releases'
+fi
 
 function build() {
-    pdk build
+    pdk build --force
 }
 
 function upload() {
-    curl -D- --fail --request 'POST' "$REPOSITORY_URL" \
-      -F file="@${NAME}".tar.gz \
-      -H "Authorization: Bearer $FORGE_API_KEY"
+    curl \
+        --silent \
+        --max-time 5 \
+        --show-error \
+        --connect-timeout 5 \
+        --request 'POST' \
+        --form file="@$NAME".tar.gz \
+        --header 'Content-Type: multipart/form-data' \
+        --header "Authorization: Bearer $FORGE_API_KEY" \
+        "$REPOSITORY_URL"
+
+    if [ 0 -eq $? ]; then
+        exit 1
+    fi
 }
 
 build
